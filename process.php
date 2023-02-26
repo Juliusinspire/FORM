3ea86b2fce5e07d0d655b051d10ed310b4d17575
Juliusinspire <juliusortega2000@gmail.com>
2023-02-26 17:24:09 +0100

    My project

---
+++ process.php
@@ -0,0 +1,58 @@
+<!-- <!DOCTYPE html>
+<html>
+
+<head>
+    <title>table</title>
+</head>
+
+<body>
+    <center> -->
+        //<?php
+
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        // $conn = mysqli_connect("localhost", "root", "", "tegx");
+        
+        // // Check connection
+        // if($conn === false){
+        //     die("ERROR: Could not connect. "
+        //         . mysqli_connect_error());
+        // }
+        
+        // Taking all 5 values from the form data(input)
+        
+        // $a = $_REQUEST['a'];
+        // $b = $_REQUEST['b'];
+        // $c = $_REQUEST['c'];
+        // $d = $_REQUEST['d'];
+        //$email = $_REQUEST['email'];
+        
+        // Performing insert query execution
+        // here our table name is college
+        
+//         $sql = "INSERT INTO user1 (a, b, c, d) 
+//         VALUES ('$a', '$b', '$c', '$d')";
+
+
+
+//         if(mysqli_query($conn, $sql)){
+//             echo "<h3>data stored in a database successfully."
+//                 . " Please browse your localhost php my admin"
+//                 . " to view the updated data</h3>";
+
+//             echo nl2br("$a $b "
+//                 . "$c $d");
+//         } else{
+//             echo "ERROR: Hush! Sorry $sql. "
+//                 . mysqli_error($conn);
+//         }
+        
+//         // Close connection
+//         mysqli_close($conn);
+//         ?>
+//    <!--  </center>
+// </body>
+
+// </html> -->
